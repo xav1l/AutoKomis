b5484cdb01f2ba0a11b100f77e9010129f704371
xav1l <x.suska@gmail.com>
2018-09-23 11:01:59 +0200

    initial version

---
+++ src/main/java/pl/pawelsuska/AutoKomis/AutoKomisApplication.java
@@ -0,0 +1,12 @@
+package pl.pawelsuska.AutoKomis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoKomisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoKomisApplication.class, args);
+	}
+}
